782f1a893005ce5d91837663183735d1984b4810
Christoph Cudly <christoph.cudly@gmail.com>
2018-06-15 18:33:50 +0200

    x64

--- OpenGLGame/main.cpp
+++ OpenGLGame/main.cpp
@@ -6,19 +6,47 @@
 #include <GL/gl.h>  
 #include <GL/glu.h>  
 
+#include <stdio.h>
 #include <stdlib.h> // for exit
+//#include <string>
+//#include <iostream>
+//#include <fstream>
 
-int window;
+#include "tga.h"
+
+#include <math.h>
 
 void hideConsole();
 
-void display(void)
+using namespace std;
+
+/* some math.h files don't define pi...*/
+#ifndef M_PI
+#define M_PI 3.141592653
+#endif
+
+#define RAD(x) (((x)*M_PI)/180.)
+
+int window;
+float move = 0.0f;
+GLuint texture;
+int animating = 1;
+
+int moving = 0;     /* flag that is true while mouse moves */
+int begin_x = 0;        /* x value of mouse movement */
+int begin_y = 0;      /* y value of mouse movement */
+GLfloat angle_y = 0;  /* angle of spin around y axis of scene, in degrees */
+GLfloat angle_x = 0;  /* angle of spin around x axis  of scene, in degrees */
+
+void reportGLError(const char * msg)
 {
-	glLoadIdentity();
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glTranslatef(0.0f, 0.0f, -6.0f);
-	glutSolidSphere(1.0, 200, 160);
-	glutSwapBuffers();
+	GLenum errCode;
+	const GLubyte *errString;
+	while ((errCode = glGetError()) != GL_NO_ERROR) {
+		errString = gluErrorString(errCode);
+		fprintf(stderr, "OpenGL Error: %s %s\n", msg, errString);
+	}
+	return;
 }
 
 void resize(int width, int height)
@@ -27,43 +55,222 @@ void resize(int width, int height)
 	if (height == 0) { height = 1; }
 
 	glViewport(0, 0, width, height);
+
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	gluPerspective(45.0f, (GLfloat)width / (GLfloat)height, 0.1f, 100.0f);
+	gluPerspective(45.0f, (float)width / (float)height, 0.1f, 100.0f);
 	glMatrixMode(GL_MODELVIEW);
+}
+
+void specialKeyPressed(int key, int x, int y)
+{
+	switch (key) {
+
+	case GLUT_KEY_UP:     /* <cursor up> */
+		move += 0.1f;
+		glutPostRedisplay();
+		break;
+	case GLUT_KEY_DOWN:     /* <cursor down> */
+		move -= 0.1f;
+		glutPostRedisplay();
+		break;
+	}
+}
+
+void keyPressed(unsigned char key, int x, int y)
+{
+	switch (key) {
+	case 27:
+		glutDestroyWindow(window);
+		exit(0);
+		break;
+	case 'a':
+		animating = animating ? 0 : 1;
+		glutPostRedisplay();
+		break;
+	default:
+		break;
+	}
+}
+
+void drawCube()
+{
+	glBegin(GL_QUADS);
+	// front face
+	glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, -1.0f, 1.0f);
+	glTexCoord2f(1.0f, 0.0f); glVertex3f(1.0f, -1.0f, 1.0f);
+	glTexCoord2f(1.0f, 1.0f); glVertex3f(1.0f, 1.0f, 1.0f);
+	glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f, 1.0f, 1.0f);
+	// back face
+	glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f, -1.0f);
+	glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f, 1.0f, -1.0f);
+	glTexCoord2f(0.0f, 1.0f); glVertex3f(1.0f, 1.0f, -1.0f);
+	glTexCoord2f(0.0f, 0.0f); glVertex3f(1.0f, -1.0f, -1.0f);
+	// top face
+	glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f, 1.0f, -1.0f);
+	glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, 1.0f, 1.0f);
+	glTexCoord2f(1.0f, 0.0f); glVertex3f(1.0f, 1.0f, 1.0f);
+	glTexCoord2f(1.0f, 1.0f); glVertex3f(1.0f, 1.0f, -1.0f);
+	// bottom face
+	glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f, -1.0f, -1.0f);
+	glTexCoord2f(0.0f, 1.0f); glVertex3f(1.0f, -1.0f, -1.0f);
+	glTexCoord2f(0.0f, 0.0f); glVertex3f(1.0f, -1.0f, 1.0f);
+	glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f, 1.0f);
+	// right face
+	glTexCoord2f(1.0f, 0.0f); glVertex3f(1.0f, -1.0f, -1.0f);
+	glTexCoord2f(1.0f, 1.0f); glVertex3f(1.0f, 1.0f, -1.0f);
+	glTexCoord2f(0.0f, 1.0f); glVertex3f(1.0f, 1.0f, 1.0f);
+	glTexCoord2f(0.0f, 0.0f); glVertex3f(1.0f, -1.0f, 1.0f);
+	// left face
+	glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, -1.0f, -1.0f);
+	glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f, 1.0f);
+	glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f, 1.0f, 1.0f);
+	glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f, 1.0f, -1.0f);
+	glEnd();
+}
+
+void display()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glLoadIdentity();
+
+	gluLookAt(-sinf(RAD(angle_y)), sinf(RAD(angle_x)), cosf(RAD(angle_y)),
+		0., 0., 0.,
+		0., 1., 0.);
+
+	glEnable(GL_TEXTURE_2D);
+	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	glTranslatef(0, 0, -move);
+
+	glPushMatrix();
+	glTranslatef(-2, 0, 0);
+	glTranslatef(0, 0, 4);
+	drawCube();
+	glTranslatef(0, 0, -4);
+	drawCube();
+	glTranslatef(0, 0, -4);
+	drawCube();
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(2, 0, 0);
+	glTranslatef(0, 0, 4);
+	drawCube();
+	glTranslatef(0, 0, -4);
+	drawCube();
+	glTranslatef(0, 0, -4);
+	drawCube();
+	glPopMatrix();
+
+	glDisable(GL_TEXTURE_2D);
+
+	glutSwapBuffers();
 }
 
 void init(int width, int height)
 {
-	GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
-	GLfloat mat_shininess[] = { 5.0 };
-	GLfloat light_position[] = { 1.0, 1.0, 1.0, 0.0 };
-
-	glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
-	glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
-	glLightfv(GL_LIGHT0, GL_POSITION, light_position);
-
-	glEnable(GL_LIGHTING);
-	glEnable(GL_LIGHT0);
-	glEnable(GL_DEPTH_TEST);
-
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 	glClearDepth(1.0);
 	glDepthFunc(GL_LESS);
 	glEnable(GL_DEPTH_TEST);
 	glShadeModel(GL_SMOOTH);
+
 	resize(width, height);
+
+	GLsizei w, h;
+	tgaInfo *info = 0;
+	int mode;
+
+	info = tgaLoad("crate.tga");
+
+	if (info->status != TGA_OK) {
+		fprintf(stderr, "error loading texture image: %d\n", info->status);
+
+		return;
+	}
+	if (info->width != info->height) {
+		fprintf(stderr, "Image size %d x %d is not rectangular, giving up.\n",
+			info->width, info->height);
+		return;
+	}
+
+	mode = info->pixelDepth / 8;  // will be 3 for rgb, 4 for rgba
+	glGenTextures(1, &texture);
+
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
+
+	// Upload the texture bitmap. 
+	w = info->width;
+	h = info->height;
+
+	reportGLError("before uploading texture");
+	GLint format = (mode == 4) ? GL_RGBA : GL_RGB;
+	glTexImage2D(GL_TEXTURE_2D, 0, format, w, h, 0, format,
+		GL_UNSIGNED_BYTE, info->imageData);
+	reportGLError("after uploading texture");
+
+	tgaDestroy(info);
+
 }
 
-void keyPressed(unsigned char key, int x, int y)
+void timer(int value)
 {
-	if (key == 27) {
-		glutDestroyWindow(window);
-		exit(0);
+	glutPostRedisplay();
+	glutTimerFunc(15, timer, 1);
+}
+
+
+void mouse(int button, int state, int x, int y)
+{
+	switch (button) {
+	case GLUT_LEFT_BUTTON:    /* spin scene around */
+		if (state == GLUT_DOWN) {
+			moving = 1;
+			begin_x = x;
+			begin_y = y;
+
+		}
+		else if (state == GLUT_UP) {
+			moving = 0;
+		}
+		break;
+
+	default:
+		break;
+	}
+
+	glutPostRedisplay();
+}
+
+
+void mouseMotion(int x, int y) {
+
+	if (moving) { /* mouse button is pressed */
+
+				  /* calculate new modelview matrix values */
+		angle_y = angle_y + (x - begin_x);
+		angle_x = angle_x + (y - begin_y);
+		if (angle_x > 360.0) angle_x -= 360.0;
+		else if (angle_x < -360.0) angle_x += 360.0;
+		if (angle_y > 360.0) angle_y -= 360.0;
+		else if (angle_y < -360.0) angle_y += 360.0;
+
+		begin_x = x;
+		begin_y = y;
+		glutPostRedisplay();
 	}
 }
 
-int main(int argc, char** argv)
+
+int main(int argc, char **argv)
 {
 	hideConsole();
 
@@ -75,7 +282,12 @@ int main(int argc, char** argv)
 	glutDisplayFunc(&display);
 	glutReshapeFunc(&resize);
 	glutKeyboardFunc(&keyPressed);
+	glutSpecialFunc(&specialKeyPressed);
 	init(640, 480);
+	glutTimerFunc(15, timer, 1);
+	glutMouseFunc(mouse);
+	glutMotionFunc(mouseMotion);
+	glutFullScreen();
 	glutMainLoop();
 	return 0;
 }
---
+++ OpenGLGame/wall.h
@@ -0,0 +1,7 @@
+
+#ifndef WALL_H
+#define WALL_H 
+
+
+
+#endif // !WALL_H
