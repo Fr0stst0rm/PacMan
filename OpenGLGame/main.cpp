d55e88ba55438df91f8802181bb14b34bed83823
Alex Schaller <schaller.alexander@protonmail.com>
2018-06-20 15:15:14 +0200

    main updated, unneeded stuff removed

--- OpenGLGame/main.cpp
+++ OpenGLGame/main.cpp
@@ -8,7 +8,7 @@
 #include <stdlib.h>
 
 #include <math.h>
-
+#include "map.h"
 #include "wall.h"
 
 void glutLeaveMainLoop();
@@ -28,16 +28,13 @@ float movePlayer = 0.0f;
 
 int animating = 1;
 
-int moving = 0;     /* flag that is true while mouse moves */
-int begin_x = 0;        /* x value of mouse movement */
-int begin_y = 0;      /* y value of mouse movement */
-GLfloat angle_y = 0;  /* angle of spin around y axis of scene, in degrees */
-GLfloat angle_x = 0;  /* angle of spin around x axis  of scene, in degrees */
+float moving = 0;     /* flag that is true while mouse moves */
 
-					  //Game Test
 Wall * wall1;
 Wall * wall2;
 Wall * wall3;
+
+Map* map;
 
 void reportGLError(const char * msg)
 {
@@ -63,21 +60,6 @@ void resize(int width, int height)
 	glMatrixMode(GL_MODELVIEW);
 }
 
-void specialKeyPressed(int key, int x, int y)
-{
-	switch (key) {
-
-	case GLUT_KEY_UP:     /* <cursor up> */
-		movePlayer += 0.1f;
-		glutPostRedisplay();
-		break;
-	case GLUT_KEY_DOWN:     /* <cursor down> */
-		movePlayer -= 0.1f;
-		glutPostRedisplay();
-		break;
-	}
-}
-
 void keyPressed(unsigned char key, int x, int y)
 {
 	switch (key) {
@@ -99,17 +81,13 @@ void display()
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glLoadIdentity();
 
-	gluLookAt(-sinf(RAD(angle_y)), sinf(RAD(angle_x)), cosf(RAD(angle_y)), 0., 0., 0., 0., 1., 0.);
-
-	glTranslatef(0, 0, -movePlayer);
-
-	wall1->setPos(-2, 0, -4);
+	wall1->setPos(-2, -movePlayer, -10);
 	wall1->draw();
 
-	wall2->setPos(0, 0, -4);
+	wall2->setPos(0, 0, -10);
 	wall2->draw();
 
-	wall3->setPos(2, 0, -4);
+	wall3->setPos(2, 0, -10);
 	wall3->draw();
 
 	glutSwapBuffers();
@@ -125,6 +103,14 @@ void init(int width, int height)
 
 	resize(width, height);
 
+	/*map = new Map();
+	map->loadMap();*/
+
+	
+
+	// ####################################################################
+	// TEST VARIABLES -- DELETE LATER ON
+	// ####################################################################
 	wall1 = new Wall(1.1f);
 	wall1->setTexture("crate.tga");
 
@@ -132,7 +118,7 @@ void init(int width, int height)
 	wall2->setTexture("cheese.tga");
 
 	wall3 = new Wall(1.3);
-	wall3->setTexture("elf.tga");
+	wall3->setTexture("wall.tga");
 
 }
 
@@ -140,48 +126,6 @@ void timer(int value)
 {
 	glutPostRedisplay();
 	glutTimerFunc(15, timer, 1);
-}
-
-
-void mouse(int button, int state, int x, int y)
-{
-	switch (button) {
-	case GLUT_LEFT_BUTTON:    /* spin scene around */
-		if (state == GLUT_DOWN) {
-			moving = 1;
-			begin_x = x;
-			begin_y = y;
-
-		}
-		else if (state == GLUT_UP) {
-			moving = 0;
-		}
-		break;
-
-	default:
-		break;
-	}
-
-	glutPostRedisplay();
-}
-
-
-void mouseMotion(int x, int y) {
-
-	if (moving) { /* mouse button is pressed */
-
-				  /* calculate new modelview matrix values */
-		angle_y = angle_y + (x - begin_x);
-		angle_x = angle_x + (y - begin_y);
-		if (angle_x > 360.0) angle_x -= 360.0;
-		else if (angle_x < -360.0) angle_x += 360.0;
-		if (angle_y > 360.0) angle_y -= 360.0;
-		else if (angle_y < -360.0) angle_y += 360.0;
-
-		begin_x = x;
-		begin_y = y;
-		glutPostRedisplay();
-	}
 }
 
 
@@ -197,11 +141,8 @@ int main(int argc, char **argv)
 	glutDisplayFunc(&display);
 	glutReshapeFunc(&resize);
 	glutKeyboardFunc(&keyPressed);
-	glutSpecialFunc(&specialKeyPressed);
 	init(640, 480);
 	glutTimerFunc(15, timer, 1);
-	glutMouseFunc(mouse);
-	glutMotionFunc(mouseMotion);
 	//glutFullScreen();
 	glutMainLoop();
 	glutLeaveMainLoop();
